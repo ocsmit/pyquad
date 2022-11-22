740c14fd4085af0ee25f1a19524176471fb35671
ocsmit <osmith2@ncsu.edu>
2022-11-22 16:27:52 -0500

    Fix package

--- spadmon/__init__.py
+++ spadmon/__init__.py
@@ -1,5 +1,7 @@
 from .geometry_objects import BoundingBox, Point
-from .region_quadtree import RegionQuadTree
+from .region_quadtree import RegionQuadTree, RegionNode
 from .point_quadtree import PointQuadTree
+
+# from .node import Node
 
 __all__ = ["PointQuadTree", "RegionQuadTree"]
